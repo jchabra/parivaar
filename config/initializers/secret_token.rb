d745fd30aafe4799d5f4cbd7409d81c1eba4f549
Jasmine Chabra <jchabra@gmail.com>
2013-07-09 15:47:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parivaar::Application.config.secret_token = '1e1ac10e79783ed38caec41a432bfa76e2488652dd55705fcf252f066e636c371903fa10282ea54ab5f7b7e50cb4be250fbb0ba1335569468e947d9bd3a739e5'
